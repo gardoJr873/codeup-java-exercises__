a94c30d9f071b7f83b0a065572c13bfbe0925e9e
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-01-29 13:13:17 -0600

    Console exercise progress submission

--- ConsoleExercises.java
+++ ConsoleExercises.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
+
 public class ConsoleExercises {
     public static void main(String[] args) {
-
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately" + " %f", pi);
     }
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,67 +1,67 @@
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.print("Hello,");
-        System.out.println(" World!");
-
-        //added escape sequence initially to compensate for output of
-        // myFavoriteNumber displaying on same line as Hello World but
-        // changed preceding print method to println which corrected problem
-        int myFavoriteNumber = 74;
-        System.out.println(myFavoriteNumber);
-
-        //no syntax errors encountered
-        String myString = "3.14159";
-        System.out.print(myString);
-
-        //myNumber not initialized error;
-        //myNumber when assigned 3.14 references casting long to double i.e. incompatible types.
-        //incompatible types when assigned the data type float
-        float myNumber = 3.14;
-        System.out.print(myNumber);
-
-        //the post increment for x adds one to value i.e. 5 + 1 changing value after
-        int x = 5;
-        System.out.println(x++);
-        System.out.println(x);
-
-        //the pre increment for y(x) adds one before changing value initially 1 + 5
-        int y = 5;
-        System.out.println(++y);
-        System.out.println(y);
-
-        //output delcares java: not a statement
-        String class = "Duff";
-        System.out.println(class);
-
-        //type conversion errors
-        String theNumberThree = "three";
-        Object o = theNumberThree;
-        int three = (int) "three";
-
-        //exercise 13
-        int x = 4;
-        x = x + 5;
-        System.out.println(x+=5);
-
-        int r = 3;
-        int y = 4;
-        y = y * r;
-        System.out.println(y*=r);
-
-        int w = 10;
-        int s = 2;
-        w = w / s;
-        System.out.println(w/=s);
-        System.out.println(s-=w);
-
-        //exercise 14
-        byte bigNumber = 129;
-        System.out.println(bigNumber);
-        //lossy conversion from int to byte
-
-        short biggerNumber;
-        biggerNumber = 32767;
-        System.out.println(++biggerNumber);
-        //console output returns negative number whether byte, short, int, long
-    }
-}
+//public class HelloWorld {
+//    public static void main(String[] args){
+//        System.out.print("Hello,");
+//        System.out.println(" World!");
+//
+//        //added escape sequence initially to compensate for output of
+//        // myFavoriteNumber displaying on same line as Hello World but
+//        // changed preceding print method to println which corrected problem
+//        int myFavoriteNumber = 74;
+//        System.out.println(myFavoriteNumber);
+//
+//        //no syntax errors encountered
+//        String myString = "3.14159";
+//        System.out.print(myString);
+//
+//        //myNumber not initialized error;
+//        //myNumber when assigned 3.14 references casting long to double i.e. incompatible types.
+//        //incompatible types when assigned the data type float
+//        float myNumber = 3.14;
+//        System.out.print(myNumber);
+//
+//        //the post increment for x adds one to value i.e. 5 + 1 changing value after
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        //the pre increment for y(x) adds one before changing value initially 1 + 5
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+//
+//        //output delcares java: not a statement
+//        String class = "Duff";
+//        System.out.println(class);
+//
+//        //type conversion errors
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//
+//        //exercise 13
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x+=5);
+//
+//        int r = 3;
+//        int y = 4;
+//        y = y * r;
+//        System.out.println(y*=r);
+//
+//        int w = 10;
+//        int s = 2;
+//        w = w / s;
+//        System.out.println(w/=s);
+//        System.out.println(s-=w);
+//
+//        //exercise 14
+//        byte bigNumber = 129;
+//        System.out.println(bigNumber);
+//        //lossy conversion from int to byte
+//
+//        short biggerNumber;
+//        biggerNumber = 32767;
+//        System.out.println(++biggerNumber);
+//        //console output returns negative number whether byte, short, int, long
+//    }
+//}
