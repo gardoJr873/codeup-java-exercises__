5e428999e020d2f5606bbda8ecc12b8c7d6ea60d
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-01-29 13:13:30 -0600

    Console exercise progress submission

--- ConsoleExercises.java
+++ ConsoleExercises.java
@@ -5,5 +5,14 @@ public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
         System.out.format("The value of pi is approximately" + " %f", pi);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number of your choice:  ");
+        Number userInput = scanner.nextInt();
+        //if input is not a number exception thread is thrown indicating InputMismatchException
+
+        System.out.print("You entered: " + userInput);
     }
 }
